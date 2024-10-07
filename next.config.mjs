3c8c2ca1b3be7618b7642754446740ef2bf0fe84
Ubuntu <ubuntu@vps-b7e44729.vps.ovh.net>
2024-10-07 14:00:52 +0000

    next-config

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/admin',
+            destination: '/dashboard/home',
+            permanent: true,
+          }
+        ]
+      },
+      images: {
+        remotePatterns: [
+          {
+            protocol: 'http',
+            hostname: '176.31.163.243', 
+            pathname: '/recipe_img/**', 
+          },
+          {
+            protocol: 'http',
+            hostname: '176.31.163.243',
+            pathname: '/tips_img/**',
+          },
+        ],
+      },
+};
+
+export default nextConfig;
